4aa6409ae540ab1be3afe8782b1958564afbb5ca
Fergus Leen <fergusleen@gmail.com>
2023-12-12 01:34:00 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 cd bin
 ../rasm -s ../src/termM4.s
 ../rasm ../src/charset.s
-../rasm ../src/telnet.s
-cp ./EWENM4.BIN ~/Library/"Application Support"/CPCemu/SDCARD/.
-cp ./TELNETE.BIN ~/Library/"Application Support"/CPCemu/SDCARD/.
-cp ./CHARSET.BIN ~/Library/"Application Support"/CPCemu/SDCARD/.
-cd ..
+
